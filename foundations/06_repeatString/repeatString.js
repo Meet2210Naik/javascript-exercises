0e4361f9dfcb91cdaac5f10e377cc644d50eb397
Meet Naik <meet.naikk@gmail.com>
2026-01-29 02:21:17 -0500

    Complete repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+
+
+const repeatString = function(string,numOfTimes) {
+    
+    let word ='';
+    if(numOfTimes >= 0){
+        for(let i = 0; i < numOfTimes ; i++)
+            {
+              word += string;
+            }
+            return word;
+    }
+    else{
+       return string = "ERROR";
+    }
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
