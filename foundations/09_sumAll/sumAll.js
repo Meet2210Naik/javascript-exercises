c4d7a5ef2e55801422ed2d0cacfb2bf32865a717
Meet Naik <meet.naikk@gmail.com>
2026-01-29 03:17:14 -0500

    Complete sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(begin,end) {
+    let sum = 0;
+    if(begin >= 0  && end >= 0 && Number.isInteger(begin) && Number.isInteger(end)){
+        if(begin < end){
+             for(let i = begin ; i <= end ; i++ )
+                {
+                    sum += i;
+                }
+            return sum;
+        }
+        else if(begin > end) 
+          {
+              for(let i = end ; i <= begin ; i++ )
+                  {
+                      sum += i;
+                  }
+              return sum; 
+          }
+      } 
+      else
+        {
+            return "ERROR";
+        } 
 };
 
 // Do not edit below this line
