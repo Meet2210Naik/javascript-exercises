582f9eda0ebe5cbba1b6ae4188c00faa4b0be0bc
Meet Naik <meet.naikk@gmail.com>
2026-01-29 02:54:06 -0500

    Complete removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray,num1,num2,num3,num4) {
+
+    let size = myArray.length;
+    for(let i = size - 1; i >=  0 ; i--)
+        {
+            
+            if(myArray[i] === num1 || myArray[i] === num2 || myArray[i] === num3 || myArray[i] === num4)
+                {
+                    myArray.splice(i,1);
+                }
+        }
+        
+        return myArray;
 };
 
 // Do not edit below this line
