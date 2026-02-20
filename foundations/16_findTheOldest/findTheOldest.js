cf7639b6adcc69c7804f5bbaed7d767a5748f16e
Meet Naik <meet.naikk@gmail.com>
2026-02-20 16:11:27 -0500

    Complete findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let today  = new Date();
+    let maxAge = (people[0].yearOfDeath ?? today.getFullYear()) - people[0].yearOfBirth ;
+    let age = 0;
+    let eldest = people[0];
 
+    for(let i = 1 ; i <  people.length ;i++)
+        {
+            age = (people[i].yearOfDeath ?? today.getFullYear()) - people[i].yearOfBirth;
+
+            if(maxAge < age){
+                maxAge = age;
+                eldest = people[i];     
+            }
+            
+        }
+
+        return eldest;
+        
 };
 
 // Do not edit below this line
