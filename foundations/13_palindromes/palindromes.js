9ffbe5e3fe9bf823bd6e73c5ed51809d6864da8b
Meet Naik <meet.naikk@gmail.com>
2026-02-20 15:25:59 -0500

    Complete palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) 
+{
+    const cleanstring = string.toLowerCase().replace(/[^a-z0-9]/g,'');
+    let left = 0;
+    let right = cleanstring.length - 1;
 
+    
+    while(left < right){
+        if(cleanstring[left] !== cleanstring[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+    }
+    return true;
+    
+    
 };
 
 // Do not edit below this line
