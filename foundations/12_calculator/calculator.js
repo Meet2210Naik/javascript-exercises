d9cd114dbdfae998665af820a20b6309e074d903
Meet Naik <meet.naikk@gmail.com>
2026-02-20 15:02:52 -0500

    Complete Calcualtor exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
+const add = function(a,b) {
 	
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(myarray) {
+  let sum = 0;
+	for(let i = 0; i < myarray.length;i++){
+    sum += myarray[i];
+  }
+
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function(myarray) {
+  let product = myarray[0];
+  for(let i = 1 ;i < myarray.length ;i++){
+    product *= myarray[i];
+  }
 
+    return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let power = 1;
+  for(let i = 1 ; i <= b ; i++)
+    {
+      power *= a;
+    }
+
+    return power;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  let fac = 1;
+  if(a === 0 || a === 1)
+    return 1;
+
+ 
+  for (let i = 2; i <= a; i++) {
+    fac *= i;
+  }
+  return fac;
+};  
 
 // Do not edit below this line
 module.exports = {
