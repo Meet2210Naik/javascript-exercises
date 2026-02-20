df7e3803dd8c376e657707b0d6eeb2f0ee36654c
Meet Naik <meet.naikk@gmail.com>
2026-02-20 15:24:20 -0500

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+      if (index < 0) return "OOPS";
+  if (index === 0) return 0;
+  if (index === 1) return 1;
 
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 2; i <= index; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
